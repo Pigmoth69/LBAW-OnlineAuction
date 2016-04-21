c8b43c3f0e020ae9e129e8735b01a650a348f103
Miguel Botelho <m_botelho8@hotmail.com>
2016-04-21 21:42:41 +0100

    update indexes

--- sql/tables.sql
+++ sql/tables.sql
@@ -243,9 +243,7 @@ CREATE UNIQUE INDEX Index_e_mail ON Utilizador USING btree (e_mail);
 
 CREATE UNIQUE INDEX Index_pais ON Pais USING btree (nome_pais);
 
-CREATE INDEX leilao_text ON Leilao USING gin(to_tsvector('portuguese', descricao));
-
-CREATE INDEX Leilao_nome ON Leilao USING gin(to_tsvector('portuguese', nome_produto));
+CREATE INDEX leilao_text ON Leilao USING gin(to_tsvector('portuguese', descricao || ' ' || nome_produto));
 
 CREATE INDEX Utilizador_nome ON Utilizador USING gin(to_tsvector('portuguese', nome));
 
