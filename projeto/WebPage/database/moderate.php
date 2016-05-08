86364ba2b221d7929aa88e7b362bd632407b76e5
Miguel Botelho <m_botelho8@hotmail.com>
2016-05-08 01:52:20 +0100

    surrounded with try catch

--- projeto/WebPage/database/moderate.php
+++ projeto/WebPage/database/moderate.php
@@ -38,22 +38,29 @@
         if (count($mods) == 0) {
             return false;
         }
+        
         $conn->beginTransaction();
-        $stmt = $conn->prepare('UPDATE HistoricoBanidos SET id_moderador = :id_mod WHERE id_moderador = :id');
-        $stmt->bindParam(':id', $id);
-        $stmt->bindParam(':id_mod', $mods[0]['id_utilizador']);
-        $stmt->execute();
-        
-        $stmt = $conn->prepare('UPDATE Leilao SET id_moderador = :id_mod WHERE id_moderador = :id');
-        $stmt->bindParam(':id', $id);
-        $stmt->bindParam(':id_mod', $mods[0]['id_utilizador']);
-        $stmt->execute();
-        
-        $stmt = $conn->prepare('DELETE FROM UtilizadorModerador WHERE id_utilizador = :id');
-        $stmt->bindParam(':id', $id, PDO::PARAM_INT);
-        $retorno = $stmt->execute();
-        $conn->commit();
-        return $retorno;
+        try {
+            $stmt = $conn->prepare('UPDATE HistoricoBanidos SET id_moderador = :id_mod WHERE id_moderador = :id');
+            $stmt->bindParam(':id', $id);
+            $stmt->bindParam(':id_mod', $mods[0]['id_utilizador']);
+            $stmt->execute();
+            
+            $stmt = $conn->prepare('UPDATE Leilao SET id_moderador = :id_mod WHERE id_moderador = :id');
+            $stmt->bindParam(':id', $id);
+            $stmt->bindParam(':id_mod', $mods[0]['id_utilizador']);
+            $stmt->execute();
+            
+            $stmt = $conn->prepare('DELETE FROM UtilizadorModerador WHERE id_utilizador = :id');
+            $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+            $retorno = $stmt->execute();
+            $conn->commit();
+            return $retorno;
+        }
+        catch (Exception $e) {
+            echo $e;
+            $conn->rollBack();
+        }
     }
     
     function registerMod($name, $date, $gender, $mail, $password, $pais) {
@@ -81,30 +88,35 @@
         $pass = hash("sha256", $password);
         
         $conn->beginTransaction();
-        
-        $stmt = $conn->prepare('INSERT INTO Utilizador(nome, descricao, genero, imagem_utilizador, datanasc, e_mail, password, classificacao, banido, id_pais) VALUES(:nome, :descricao, :genero, :imagem_utilizador, :datanasc, :e_mail, :password, :classificacao, :banido, :id_pais)');
-        $stmt->bindParam(':nome', $name, PDO::PARAM_STR);
-        $stmt->bindParam(':descricao', $empty, PDO::PARAM_STR);
-        $stmt->bindParam(':genero', $gender, PDO::PARAM_STR);
-        $stmt->bindParam(':imagem_utilizador', $empty, PDO::PARAM_STR);
-        $stmt->bindParam(':datanasc', $date, PDO::PARAM_STR);
-        $stmt->bindParam(':e_mail', $mail, PDO::PARAM_STR);
-        $stmt->bindParam(':password', $pass, PDO::PARAM_STR);
-        $stmt->bindParam(':classificacao', $zero, PDO::PARAM_INT);
-        $stmt->bindParam(':banido', $false, PDO::PARAM_BOOL);
-        $stmt->bindParam(':id_pais', $id_pais, PDO::PARAM_INT);
-        $stmt->execute();
-        
-        $stmt = $conn->prepare('SELECT * FROM Utilizador WHERE e_mail = :mail');
-        $stmt->bindParam('mail', $mail, PDO::PARAM_STR);
-        $stmt->execute();
-        $result = $stmt->fetchAll();
-        $id = $result[0]['id_utilizador'];
-        
-        $stmt = $conn->prepare('INSERT INTO UtilizadorModerador(id_utilizador) VALUES(:id)');
-        $stmt->bindParam('id', $id, PDO::PARAM_INT);
-        $retorno = $stmt->execute();
-        $conn->commit();
-        return $retorno;
+        try {
+            $stmt = $conn->prepare('INSERT INTO Utilizador(nome, descricao, genero, imagem_utilizador, datanasc, e_mail, password, classificacao, banido, id_pais) VALUES(:nome, :descricao, :genero, :imagem_utilizador, :datanasc, :e_mail, :password, :classificacao, :banido, :id_pais)');
+            $stmt->bindParam(':nome', $name, PDO::PARAM_STR);
+            $stmt->bindParam(':descricao', $empty, PDO::PARAM_STR);
+            $stmt->bindParam(':genero', $gender, PDO::PARAM_STR);
+            $stmt->bindParam(':imagem_utilizador', $empty, PDO::PARAM_STR);
+            $stmt->bindParam(':datanasc', $date, PDO::PARAM_STR);
+            $stmt->bindParam(':e_mail', $mail, PDO::PARAM_STR);
+            $stmt->bindParam(':password', $pass, PDO::PARAM_STR);
+            $stmt->bindParam(':classificacao', $zero, PDO::PARAM_INT);
+            $stmt->bindParam(':banido', $false, PDO::PARAM_BOOL);
+            $stmt->bindParam(':id_pais', $id_pais, PDO::PARAM_INT);
+            $stmt->execute();
+            
+            $stmt = $conn->prepare('SELECT * FROM Utilizador WHERE e_mail = :mail');
+            $stmt->bindParam('mail', $mail, PDO::PARAM_STR);
+            $stmt->execute();
+            $result = $stmt->fetchAll();
+            $id = $result[0]['id_utilizador'];
+            
+            $stmt = $conn->prepare('INSERT INTO UtilizadorModerador(id_utilizador) VALUES(:id)');
+            $stmt->bindParam('id', $id, PDO::PARAM_INT);
+            $retorno = $stmt->execute();
+            $conn->commit();
+            return $retorno;
+        }
+        catch (Exception $e) {
+            echo $e;
+            $conn->rollBack();
+        }
     }
 ?>
