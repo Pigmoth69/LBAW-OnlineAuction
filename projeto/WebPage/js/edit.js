6b5c157052fae1e34cf3b6edde0179462fff2e65
Miguel Botelho <m_botelho8@hotmail.com>
2016-06-03 00:57:24 +0100

    dates fixed

--- projeto/WebPage/js/edit.js
+++ projeto/WebPage/js/edit.js
@@ -1,120 +1,122 @@
 $(document).ready(onReady);
 
 function onReady() {
-	$("#password_confirmationEdit").keyup(checkPasswordsEdit);
+    $("#password_confirmationEdit").keyup(checkPasswordsEdit);
     /*$("#editAdmin").submit(function(event) {
 		return checkValidity();
 	});*/
-	//$("#editUser").submit(function(){checkValidityEditUser();return false;});
-	//$("#editAdmin").submit(function(){checkValidityEdit();return false;});
-	//$("#editAuction").click(function(){checkValidityEdit();return false;});
+    //$("#editUser").submit(function(){checkValidityEditUser();return false;});
+    //$("#editAdmin").submit(function(){checkValidityEdit();return false;});
+    //$("#editAuction").click(function(){checkValidityEdit();return false;});
 };
 
 function checkPasswordsEdit() {
-	var password = $("#passwordEdit").val();
-	var password_conf = $("#password_confirmationEdit").val();
-	//falta meter aqui a segurança do número mínimo de caracteres
-	if (password != password_conf || password.length==0){
-		$("#passwordEdit").css('background-color', '#ff9a9a');
-		$("#password_confirmationEdit").css('background-color', '#ff9a9a');
-		return false;	
-	}
-	else {
-		$("#passwordEdit").css('background-color', '#a6f1a6');
-		$("#password_confirmationEdit").css('background-color', '#a6f1a6');
-		return true;
-	}
+    var password = $("#passwordEdit").val();
+    var password_conf = $("#password_confirmationEdit").val();
+    //falta meter aqui a segurança do número mínimo de caracteres
+    if (password != password_conf || password.length == 0) {
+        $("#passwordEdit").css('background-color', '#ff9a9a');
+        $("#password_confirmationEdit").css('background-color', '#ff9a9a');
+        return false;
+    } else {
+        $("#passwordEdit").css('background-color', '#a6f1a6');
+        $("#password_confirmationEdit").css('background-color', '#a6f1a6');
+        return true;
+    }
 }
 
 function checkDateEdit() {
-	
-	var today = new Date();
-	var dd = today.getDate();
-	var mm = today.getMonth()+1; //January is 0!
-	var yyyy = today.getFullYear() - 18;
 
-	if(dd<10) {
-		dd='0'+dd;
-	} 
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth() + 1; //January is 0!
+    var yyyy = today.getFullYear() - 18;
 
-	if(mm<10) {
-		mm='0'+mm;
-	} 
+    if (dd < 10) {
+        dd = '0' + dd;
+    }
 
-	today = yyyy+'/'+mm+'/'+dd;
-	
-	var years = new Date();
-	var dd = years.getDate();
-	var mm = years.getMonth()+1; //January is 0!
-	var yyyy = years.getFullYear() - 150;
+    if (mm < 10) {
+        mm = '0' + mm;
+    }
 
-	if(dd<10) {
-		dd='0'+dd;
-	} 
+    today = yyyy + '/' + mm + '/' + dd;
 
-	if(mm<10) {
-		mm='0'+mm;
-	} 
+    var years = new Date();
+    var dd1 = years.getDate();
+    var mm1 = years.getMonth() + 1; //January is 0!
+    var yyyy1 = years.getFullYear() - 150;
 
-	years = yyyy+'/'+mm+'/'+dd;
-	
-	if ($("#birthdateEdit").val() > today || $("#birthdateEdit").val() < years) {
-		$("#birthdateEdit").css('color', 'red');
-		$("#birthdateEdit").css('border-color', 'red');
-		return false;
-	}
-	else {
-		$("#birthdateEdit").css('color', 'black');
-		$("#birthdateEdit").css('border-color', '#ccc');
-		return true;
-	}
+    if (dd1 < 10) {
+        dd1 = '0' + dd1;
+    }
+
+    if (mm1 < 10) {
+        mm1 = '0' + mm1;
+    }
+
+    years = yyyy1 + '/' + mm1 + '/' + dd1;
+
+    var arr = $("#birthdateEdit").val().split("-");
+    var d = new Date(yyyy, mm, dd, 0, 0, 0, 0);
+    var d_t = new Date(arr[0], arr[1], arr[2], 0, 0, 0, 0);
+    var d_t1 = new Date(yyyy1, mm1, dd1, 0, 0, 0, 0);
+
+    if (d_t > d || d < d_t1) {
+        $("#birthdateEdit").css('color', 'red');
+        $("#birthdateEdit").css('border-color', 'red');
+        return false;
+    } else {
+        $("#birthdateEdit").css('color', 'black');
+        $("#birthdateEdit").css('border-color', '#ccc');
+        return true;
+    }
 }
 
 function checkEmptyFieldsEdit() {
-	if ($("#first_nameEdit").val() == "" || $("#last_nameEdit").val() == "" ||
-	 $("#birthdateEdit").val() == "" || $("#emailEdit").val() == "" || $("#passwordEdit").val() == "" ||
-	  $("#password_confirmationEdit").val() == "" || $("#previous_passwordEdit").val() == "") {
-		return false;
-	}
-	else return true;
+    if ($("#first_nameEdit").val() == "" || $("#last_nameEdit").val() == "" ||
+        $("#birthdateEdit").val() == "" || $("#emailEdit").val() == "" || $("#passwordEdit").val() == "" ||
+        $("#password_confirmationEdit").val() == "" || $("#previous_passwordEdit").val() == "") {
+        return false;
+    } else return true;
 }
-	
+
 function checkValidityEdit() {
-	if (!(checkPasswordsEdit())) {
-		alert("Passwords don't match.");
-		return false;
-	}
-		
-	if (!checkDateEdit()) {
-		alert("You have to be 18 years or older.");
-		return false;
-	}
-	if (!checkEmptyFieldsEdit()) {
-		alert("No empty fields allowed");	
-		return false;
-	}
-	//alert("Register successfull!!");
-	//editAdmin();
-	return true;	
+    if (!(checkPasswordsEdit())) {
+        alert("Passwords don't match.");
+        return false;
+    }
+
+    if (!checkDateEdit()) {
+        alert("You have to be 18 years or older.");
+        return false;
+    }
+    if (!checkEmptyFieldsEdit()) {
+        alert("No empty fields allowed");
+        return false;
+    }
+    //alert("Register successfull!!");
+    //editAdmin();
+    return true;
 }
 
 function checkValidityEditUser() {
-	if (!(checkPasswordsEdit())) {
-		alert("Passwords don't match.");
-		return false;
-	}
-		
-	if (!checkDateEdit()) {
-		alert("You have to be 18 years or older.");
-		return false;
-	}
-	if (!checkEmptyFieldsEdit()) {
-		alert("No empty fields allowed");	
-		return false;
-	}
-	//alert("Register successfull!!");
-	//editUser();
-	return true;	
+    if (!(checkPasswordsEdit())) {
+        alert("Passwords don't match.");
+        return false;
+    }
+
+    if (!checkDateEdit()) {
+        alert("You have to be 18 years or older.");
+        return false;
+    }
+    if (!checkEmptyFieldsEdit()) {
+        alert("No empty fields allowed");
+        return false;
+    }
+    //alert("Register successfull!!");
+    //editUser();
+    return true;
 }
 /*
 function editUser() {
